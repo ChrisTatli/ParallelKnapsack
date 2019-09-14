5067f040eafd1d7345f20176acd1d6831ba8f34a
Christopher Tatli <christophertatli@MacBook-Pro-4.local>
2019-09-14 18:31:51 +1000

    updated algorithm

--- knapsack_MPI.c
+++ knapsack_MPI.c
@@ -5,6 +5,7 @@
 #include <sys/time.h>
 #include <stdint.h>
 #include <mpi.h>
+#include <sys/resource.h>
 
 long int knapSack(long int C, long int w[], long int v[], int n);
 
@@ -15,6 +16,9 @@ uint64_t GetTimeStamp() {
 }
 
 int main(int argc, char *argv[]) {
+    struct rlimit limit;
+    getrlimit(RLIMIT_STACK, &limit);
+    printf ("\nStack Limit = %ld and %ld max\n", limit.rlim_cur, limit.rlim_max);
     long int C;    /* capacity of backpack */
     int n;    /* number of items */
     int i;    /* loop counter */
@@ -68,11 +72,23 @@ void toBinary(unsigned long long permutation, unsigned char used[], int n)
     unsigned long long highBit = 1 << (n - 1);
     while(highBit)
     {
-        unsigned long long k = (permutation & highBit ? 1 : 0);
+        unsigned char k = (permutation & highBit ? 1 : 0);
         highBit >>= 1;
         used[i] = k;
         i++;
     }
+}
+
+void print_arr(unsigned char arr[], int n)
+{
+    printf("=====================================\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%ld ", arr[i]);
+    }
+
+    printf("\n");
+    printf("=====================================\n");
 }
 
 /* (No longer from the URL given in line 2) */
@@ -81,8 +97,12 @@ long int knapSack(long int C, long int w[], long int v[], int n) {
     int rank, size;   
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
-    long int* wt = w;
-    long int* val = v;
+    long int* wt;
+    long int* val;
+    if(rank == 0){
+        wt = w;
+        val = v;
+    }
     
     if(rank != 0){
         MPI_Bcast(&C, 2, MPI_LONG, 0 , MPI_COMM_WORLD);
@@ -92,59 +112,64 @@ long int knapSack(long int C, long int w[], long int v[], int n) {
         MPI_Bcast(wt, n, MPI_LONG, 0, MPI_COMM_WORLD);
         MPI_Bcast(val, n, MPI_LONG, 0 , MPI_COMM_WORLD);
     }
-    #if 1
-    if(rank ==1 ){
-        for(int i =0; i<35; i++){
-            printf("%d, %d\n", val[i], wt[i]);
-        }
-    }
-    #endif
 
     unsigned long long permutations = pow(2.0, n);
 
     unsigned long long chunkSize = permutations / size;
     unsigned long long lowerBound = rank * chunkSize; 
-    unsigned long long upperBound = lowerBound + chunkSize;
+    unsigned long long upperBound = (lowerBound + chunkSize) - 1;
     
     unsigned long overallMaxValue = 0;
-    unsigned long  maxValue = 0;
-    for(unsigned long long p = lowerBound; p < upperBound; p++){
+    unsigned long maxValue = 0;
+               
+    unsigned char used[n];
+    unsigned char finalUsedState[n];
+
         
-        if(rank ==1){
-           // printf("%llu\n", p);
-            printf("%d\n", maxValue);
-        }
+    bzero(used, sizeof (used));
+    bzero(finalUsedState, sizeof(finalUsedState));
+    
+    toBinary(lowerBound, used, n);
+    if(rank == 0){
+    print_arr(used, n);
+    }
+    toBinary(upperBound, finalUsedState, n);
+    if(rank == 1){
+print_arr(finalUsedState, n);
+    }
+    
+    long int weight = 0;
+    long int value = 0;
+    int done = 0;
+    printf("rank:%d, size: %d, lowerbound:%llu, upperbound: %llu\n",rank, size, lowerBound, upperBound );
+    while(!done){
+
         
-        unsigned char used[n];
-        
-        bzero(used, sizeof (used));
-        toBinary(p, used, n);
-        long int weight = 0;
-        long int value = 0;
-        
-        int i = 0;
-        while (i < (n - 1)) {
-            if(used[i] == 1){
-                if((wt[i] + weight ) < C){
-                    weight += wt[i];
-                    value += val[i];
-                }
-                else{
-                    break;
-                }
+        done = 1;
+        for (int i = 0; i < n ; i++){
+            if(!used[i]){
+                used[i] = 1;
+                weight += wt[i];
+                value += val[i];
+                done = 0;
+                break;
+            } else {
+                used[i] = 0;
+                weight -= wt[i];
+                value -= val[i];
             }
-            i++;
         }
-        if (value > maxValue){
+        if(weight <= C && value > maxValue){
             maxValue = value;
         }
+        if(memcmp(used, finalUsedState, n) == 0){
+            break;
+        }
     }
+    
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Reduce(&maxValue, &overallMaxValue, 1, MPI_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    if(rank == 0){
-        return overallMaxValue;
-    } else {
-        return 0;
-    }
+    return overallMaxValue;
+
 }
